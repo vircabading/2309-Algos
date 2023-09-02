1d9ceb6ab7e4d17601cb491d538c7fd15790ac04
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-09-02 13:20:25 -0500

    create getNumPosNegZero function

--- 230902-Plus-Minus/main.js
+++ 230902-Plus-Minus/main.js
@@ -1,6 +1,17 @@
 ///////////////////////////////
 // Main Application
 
-console.log("//////////////////////////////");
-console.log("//  Plus Minus Algorithm\n\n");
+const getNumPosNegZero = (numArray) => {
+    console.log("\n*** open getNumPosNegZero() ******\n");
+    console.log({numArray});
 
+    let numPos = 0;
+    let numNeg = 0;
+    let numZero = 0;
+}
+
+console.log("//////////////////////////////");
+console.log("//  Plus Minus Algorithm\n");
+
+const A1 = [1, 1, 0, -1, -1];
+getNumPosNegZero(A1);
