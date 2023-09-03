aa4fd015e2e8e797c4d6fa798bb2cd2eb1bc8816
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-09-03 11:26:04 -0500

    add finding sum of all numbers, smallest number, and largest number in the array

--- 230903-Min-Max-Sum/main.js
+++ 230903-Min-Max-Sum/main.js
@@ -5,11 +5,21 @@
 const miniMaxSum = (arr) => {
     console.log({arr});
 
+    // Initializa Variables
     let totalSum = 0;
     let smallestInt = arr[0];
     let largestInt = arr[0];
 
+    // Iterate through array
+    // Find the smalles and largest number along with the sum of all the numbers
     for (numb of arr) {
+        if (numb < smallestInt) {
+            smallestInt = numb;
+        } else if (numb > largestInt) {
+            largestInt = numb;
+        }
+        totalSum += numb;
+
         console.log({numb});
         console.log(`total sum: ${totalSum} | smallest int: ${smallestInt} | largest int: ${largestInt}`);
     }
@@ -22,3 +32,8 @@ const a1 = [1,3,5,7,9];
 console.log("\n*********************");
 console.log(`Get Min Max Sum of ${a1}`);
 miniMaxSum(a1);
+
+const a2 = [5,3,1,9,7];
+console.log("\n*********************");
+console.log(`Get Min Max Sum of ${a2}`);
+miniMaxSum(a2);
