12880d67322190eb33c37884d673fdddeaac7fc2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-09-06 23:01:06 -0500

    create timeConversion function

--- 230904-Time-Conversion/TimeConversion.js
+++ 230904-Time-Conversion/TimeConversion.js
@@ -2,6 +2,14 @@
 //  TIME CONVERSION ALGORITHM
 /////////////////////////////////////////////////
 
-console.log("///////////////////////////////////////");
-console.log("// File Conversion Algorithm\n");
+const timeConversion = (s) => {
+    console.log({s});
+}
 
+console.log("///////////////////////////////////////");
+console.log("// Time Conversion Algorithm\n");
+
+const time1 = "12:01:00PM"
+console.log("\n*********************");
+console.log(`Time Conversion of ${time1}`);
+timeConversion(time1);
