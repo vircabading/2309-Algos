0eec6a4e4184aec015a1b0a2e0d422c3baa87412
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-09-06 23:28:26 -0500

    create convert hour function

--- 230904-Time-Conversion/TimeConversion.js
+++ 230904-Time-Conversion/TimeConversion.js
@@ -2,15 +2,22 @@
 //  TIME CONVERSION ALGORITHM
 /////////////////////////////////////////////////
 
+const convertHour = (hour,middayStat) => {
+    return hour;
+}
+
 const timeConversion = (s) => {
     console.log({s});
 
     // Parse the parts of the time
     const middayStat = s.substring(8,10);       // Find out if time is AM or PM?
     const hour = s.substring(0,2);
+    const minuteAndSeconds = s.substring(2,8);
     console.log({middayStat});
     console.log({hour});
-
+    console.log({minuteAndSeconds});
+    const newHour = convertHour(hour, middayStat);
+    return newHour + minuteAndSeconds;
 }
 
 console.log("///////////////////////////////////////");
@@ -19,15 +26,15 @@ console.log("// Time Conversion Algorithm\n");
 const time1 = "12:01:00PM"
 console.log("\n*********************");
 console.log(`Time Conversion of ${time1}`);
-timeConversion(time1);
+console.log(timeConversion(time1));
 
 const time2 = "12:01:00AM"
 console.log("\n*********************");
 console.log(`Time Conversion of ${time2}`);
-timeConversion(time2);
+console.log(timeConversion(time2));
 
 const time3 = "07:05:45PM"
 console.log("\n*********************");
 console.log(`Time Conversion of ${time3}`);
-timeConversion(time3);
+console.log(timeConversion(time3));
 
