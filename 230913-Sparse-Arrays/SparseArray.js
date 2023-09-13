170bb5cc0fe775b89cc26183466d61193c9ba8d4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-09-13 16:16:43 -0500

    add matching strings function

--- 230913-Sparse-Arrays/SparseArray.js
+++ 230913-Sparse-Arrays/SparseArray.js
@@ -2,5 +2,12 @@
 // Sparse Array Algo Application
 ///////////////////////////////////////////////
 
+function matchingStrings(strings, queries) {
+    console.log(`Strings: ${strings} | Queries: ${queries}`);
+    let results = [];
+
+    return results;
+}
+
 console.log("//////////////////////////////////");
 console.log("Sparse Array Algorithm\n");
